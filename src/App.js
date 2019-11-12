dcec084c5efc8d6ac9ee3256dcfa33e80eb4d644
Zack Kramer <zckramer83@gmail.com>
2019-11-12 17:52:09 -0500

    almost there maybe

--- src/App.js
+++ src/App.js
@@ -1,30 +1,35 @@
 import React from 'react';
+// import ReactDOM from 'react-dom';
 import './App.css';
+
 // import { stringify } from 'querystring';
 // import Http from './utils/http.js'
 
-class Recipe extends React.Component {
+const BASE_URL = "http://localhost:8080/recipes"
+
+class Recipes extends React.Component {
   state = {
     recipes: []
   }
 
   componentDidMount() {
-    fetch("http://localhost:8080/recipes")
+    fetch(BASE_URL)
     .then(res => res.json())
     .then(recipes => {
-      console.log(recipes);
-      this.setState( {recipes} )
+      // console.log(recipes);
+      // const recipes = Http.getRequest(BASE_URL)
+      this.setState({recipes: recipes})
     })
   }
 
   render() {
     return (
-      {this.state.recipes.map(recipe => (
-        <h1>{recipe.name}</h1>
-      ))}
-        
-        
-    )
+      <div>
+        {this.state.recipes.map(recipe => (
+          <h3>{recipe.name}</h3>
+        ))}
+      </div>
+    );
   }
 }
 
@@ -34,14 +39,18 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-  
+        <p>Look at AAAAAALLLLL the cocktails!</p>
       </header>
       <main className="main-body">
-        <Recipe />
+        <>
+        holy balls!
+        </>
+        {/* <Recipes /> */}
       </main>
     </div>
   );
 }
-
+// const rootElement = document.getElementById("root");
+// ReactDOM.render(<App />, rootElement);
 export default App;
 
