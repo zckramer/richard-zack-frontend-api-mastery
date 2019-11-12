f800c5b9b033483d57390941c85ddd896cc9a3ae
Zachary Kramer <zckramer83@gmail.com>
2019-11-12 18:34:12 -0500

    it works - render more now

--- src/App.js
+++ src/App.js
@@ -1,56 +1,31 @@
 import React from 'react';
-// import ReactDOM from 'react-dom';
 import './App.css';
-
-// import { stringify } from 'querystring';
-// import Http from './utils/http.js'
 
 const BASE_URL = "http://localhost:8080/recipes"
 
-class Recipes extends React.Component {
-  state = {
-    recipes: []
-  }
+function App () {
 
-  componentDidMount() {
+  // const [recipes, setRecipes] = React.useState("");
+
+  const [fetchResponse, setFetchResponse] = React.useState({});
+  
+  React.useEffect(() => {
     fetch(BASE_URL)
     .then(res => res.json())
     .then(recipes => {
-      // console.log(recipes);
-      // const recipes = Http.getRequest(BASE_URL)
-      this.setState({recipes: recipes})
+      console.log(recipes);
+      setFetchResponse(recipes)
     })
-  }
+  }, [])
 
-  render() {
-    return (
-      <div>
-        {this.state.recipes.map(recipe => (
-          <h3>{recipe.name}</h3>
-        ))}
-      </div>
-    );
-  }
-}
-
-
-
-function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>Look at AAAAAALLLLL the cocktails!</p>
-      </header>
-      <main className="main-body">
-        <>
-        holy balls!
-        </>
-        {/* <Recipes /> */}
-      </main>
+      <output>
+      {fetchResponse.Recipes && fetchResponse.Recipes[1].name}
+      </output>
     </div>
-  );
+      
+  )
 }
-// const rootElement = document.getElementById("root");
-// ReactDOM.render(<App />, rootElement);
-export default App;
 
+export default App;
