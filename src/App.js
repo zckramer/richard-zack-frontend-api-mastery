e0d494fe04f6167c60fbc615411b265ca2237b4c
Zack Kramer <zckramer83@gmail.com>
2019-11-17 15:59:38 -0500

    separated navbar and content - input fields now clear on submit - some styling

--- src/App.js
+++ src/App.js
@@ -4,31 +4,69 @@ import './App.css';
 const BASE_URL = "http://localhost:8080/recipes"
 
 function App () {
-const [state, setState] = React.useState(<Recipes />)
+  const [state, setState] = React.useState({})
+  // Return NavBar
+  React.useEffect(() => {
+    setState(<><NavBar /><Content /></>)
+  }, [])
 
-function refreshState() {
-  setState(<Recipes />)
+  return <><NavBar /><Content /></>;
 }
 
-return state;
-}
-  // const [recipes, setRecipes] = React.useState("");
+function Content () {
+  const [state, setState] = React.useState(<AllRecipes />)
 
-
+  // React.useEffect(() =>{
+  //   setState(<AllRecipes />)
+  // }, [])
   
-function Recipes () {
+  return state;
+}
+
+function NavBar () {
+  const [state, setState] = React.useState({})
+
+  return (
+    <nav className= "nav-bar"> 
+      <p className= "nav__welcome-text">Welcome! </p>
+      <p className= "nav__welcome-text">the Nav-Bar ;P</p>
+      <HomeButton />
+    </nav>
+    
+    
+  )
+}
+
+function goHome() {
+//  setState(<><NavBar /><AllRecipes /></>)
+}
+
+function HomeButton () {
+  return (
+    <button className="home-button" onClick={goHome}>All Recipes</button>
+  )
+}
+
+function AllRecipes () {
   const [fetchResponse, setFetchResponse] = React.useState({});
+  
+  function clearForm() {
+    document.querySelector(".input__recipe-name").value = ""
+    document.querySelector(".input__recipe-name").placeholder = "drop in your own recipe!"
+    document.querySelector(".input__recipe-image").value = ""
+    document.querySelector(".input__recipe-image").placeholder = "Cocktail Image URL"
+}
   function getRecipes(){
     fetch(BASE_URL)
     .then(res => res.json())
     .then(recipes => {
-      // console.log(recipes);
       setFetchResponse(recipes)
     })
+
   }
   React.useEffect(() => {
     getRecipes();
-  }, [])
+  }, [])  
   
   function handleClick(event) {
     
@@ -49,14 +87,18 @@ function Recipes () {
     })
       .then(response => {
         if(response.status === 200) {
+
+          clearForm()
+          
           getRecipes();
         }
         
       })
+
   }
   return (
     <div className="App">
-      <div className="viewport">
+      <div className="recipes-container">
       <section className="input__recipe">
         <form>
           <input className="input__recipe-name" name="name" placeholder="drop in your own recipe!"></input>
@@ -65,7 +107,6 @@ function Recipes () {
         </form>
       </section> 
       <section className="recipe-card__section">
-        
           {fetchResponse.Recipes && fetchResponse.Recipes.map(recipe =>
         <article className="recipe-card">
             <h2 className="recipe-name">{recipe.name}</h2>
@@ -76,7 +117,6 @@ function Recipes () {
       </section>
       </div>
     </div>
-      
   )
 }
 
