eacde18f4b5d6477948be78a2c3af9211304dfa1
Zack Kramer <zckramer83@gmail.com>
2019-11-17 22:05:25 -0500

    refactored to get state changes working -still can't get listeners on cards -need to make callbacks for cards, perhaps

--- src/App.js
+++ src/App.js
@@ -4,46 +4,76 @@ import './App.css';
 const BASE_URL = "http://localhost:8080/recipes"
 
 function App () {
-  const [state, setState] = React.useState({})
-  // Return NavBar
+  const [content, setContent] = React.useState(null)
+  document.querySelectorAll(".recipe-card").forEach(recipe => {
+  recipe.innerHTML = "LALA"
+  recipe.addEventListener("click", function() {
+    console.log("CLICK")
+  })
+})
+  
   React.useEffect(() => {
-    setState(<><NavBar /><Content /></>)
+   
+    setContent(<Home />)
   }, [])
 
-  return <><NavBar /><Content /></>;
-}
-
-function Content () {
-  const [state, setState] = React.useState(<AllRecipes />)
-
-  // React.useEffect(() =>{
-  //   setState(<AllRecipes />)
-  // }, [])
-  
-  return state;
-}
-
-function NavBar () {
-  const [state, setState] = React.useState({})
+  function handleEvent(prop) {
+    
+  }
+  function handleRecipesButton() {
+    setContent(<AllRecipes />)
+    }
 
   return (
+   <div>
     <nav className= "nav-bar"> 
       <p className= "nav__welcome-text">Welcome! </p>
       <p className= "nav__welcome-text">the Nav-Bar ;P</p>
-      <HomeButton />
+      <button className="recipes-button" onClick={handleRecipesButton} value="recipes">All Recipes</button>
     </nav>
+     <div>
+       {content}
+    </div>
+   </div>
+  );
+}
+
+function Home () {
+  const [state, setState] = React.useState(null)
+  
+  
+return (
+
+      <h1>Welcome to Cocktails</h1>
     
+  );
+}
+
+
+
+
+
+
+
+
+function RecipeCard (recipe) {
+  // console.log(recipe)
     
+  return (
+    <article className="recipe-card">
+    <h2 className="recipe-name">{recipe.name}</h2>
+    <img alt="Cocktail" className="recipe-image" src={recipe.image}  />
+    </article>       
   )
 }
 
-function goHome() {
-//  setState(<><NavBar /><AllRecipes /></>)
-}
-
-function HomeButton () {
-  return (
-    <button className="home-button" onClick={goHome}>All Recipes</button>
+function SingleRecipePage (recipe) {
+  return(
+    <article className="single-recipe">
+      <h2 className="single-recipe__title">{recipe.name}</h2>
+      <img alt="Cocktail" className="single-recipe__image" src ={recipe.image} />
+      
+    </article>
   )
 }
 
@@ -55,20 +85,20 @@ function AllRecipes () {
     document.querySelector(".input__recipe-name").placeholder = "drop in your own recipe!"
     document.querySelector(".input__recipe-image").value = ""
     document.querySelector(".input__recipe-image").placeholder = "Cocktail Image URL"
-}
+  }
+
   function getRecipes(){
     fetch(BASE_URL)
     .then(res => res.json())
     .then(recipes => {
       setFetchResponse(recipes)
     })
-
   }
   React.useEffect(() => {
     getRecipes();
   }, [])  
   
-  function handleClick(event) {
+  function handleSubmit(event) {
     
     event.preventDefault();
 
@@ -99,19 +129,21 @@ function AllRecipes () {
   return (
     <div className="App">
       <div className="recipes-container">
-      <section className="input__recipe">
+      <section className="recipe-form">
+        <p>New Cocktail:</p>
         <form>
           <input className="input__recipe-name" name="name" placeholder="drop in your own recipe!"></input>
           <input className="input__recipe-image" name="image" placeholder="Cocktail Image URL"></input>
-          <button type="submit" onClick={handleClick}>Create Cocktail</button>
+          <button type="submit" onClick={handleSubmit}>Create Cocktail</button>
         </form>
       </section> 
       <section className="recipe-card__section">
-          {fetchResponse.Recipes && fetchResponse.Recipes.map(recipe =>
-        <article className="recipe-card">
-            <h2 className="recipe-name">{recipe.name}</h2>
-            <img alt="Cocktail" className="recipe-image" src={recipe.image}  />
-        </article>       
+          {fetchResponse.Recipes && fetchResponse.Recipes.map(recipe => 
+          <RecipeCard 
+          name={recipe.name}    
+          image={recipe.image} 
+          recipeId={recipe._id}
+          />
             )}
         
       </section>
