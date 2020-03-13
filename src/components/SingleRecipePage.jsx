d65995f3f6ef6e7251d536fe63c21d71f1d0afb9
richard wilson <rcwilson88@gmail.com>
2020-03-13 15:06:50 -0400

    Merged from other fork

--- src/components/SingleRecipePage.jsx
+++ src/components/SingleRecipePage.jsx
@@ -8,11 +8,6 @@ export default function SingleRecipePage(props) {
 
     return (
         <div>
-            {/* <article className="recipe-card">
-                <h2 className="recipe-name">{recipe.name}</h2>
-                <img alt="Cocktail" className="recipe-image" src={recipe.image}  />
-            </article>       
- */}
         </div>
       )
 }
